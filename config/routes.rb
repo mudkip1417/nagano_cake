87cecdedfce29919e1146366b3a8b0ca1023aa40
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-07-31 01:49:10 +0000

    [Add]nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,32 @@
 Rails.application.routes.draw do
 
   root to: 'homes#top'
-  devise_for :admin
-  devise_for :customers
+
+  # 顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
 
   namespace :admin do
-    resources :genres
+    resources :genres, only: [:index,:create,:edit,:update]
   end
 
   namespace :admin do
-    resources :items
+    resources :items, only: [:index,:new,:create,:show,:edit,:update]
   end
 
   namespace :admin do
-    resources :customers
+    resources :customers, only: [:index,:create,:edit,:update]
   end
 
   namespace :admin do
-   resources :orders
+   resources :orders, only: [:show,:update]
   end
 
   namespace :admin do
