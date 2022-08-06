c0a7fd38111dbf7c0323f7663be298bc32b3ea42
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-08-06 09:01:23 +0000

    [Add]nagano_cake

--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,8 @@ module NaganoCake
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  config.i18n.default_locale = :ja
+  config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
+
 end
