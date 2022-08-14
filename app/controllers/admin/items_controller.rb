b2f56458cbb77c4b6737c8f7fd76daaa5a520497
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-08-14 13:10:10 +0000

    [Add]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,7 +10,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_homes_top_path
+    redirect_to admin_items_path
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
 end
