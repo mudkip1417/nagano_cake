4f42bc066c0f1b7da571989c1d925cf79d68cf2e
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-08-19 23:45:01 +0000

    [Add]nagano_cake

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -12,19 +12,19 @@ class Public::AddressesController < ApplicationController
     redirect_to public_addresses_path(current_customer.id)
   end
 
+  def edit
+    @address = Address.find(params[:id])
+  end
+
   def update
     @address = Address.find(params[:id])
     @address.update(address_params)
     redirect_to public_addresses_path(current_customer.id)
   end
 
-  def edit
-    @address = Address.find(params[:id])
-  end
-
   def destroy
     @address = Address.find(params[:id])
-    address.destroy
+    @address.destroy
     redirect_to public_addresses_path(current_customer.id)
   end
 
