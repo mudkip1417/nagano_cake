028adddaa7a9749465c18723b90b5564512f9fb0
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-08-20 14:42:10 +0000

    [Add]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,23 @@
 class Public::CartItemsController < ApplicationController
+
   def index
+    @cart_items = current_customer.cart_items.all
+    @items = Item.all
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to public_cart_items_path
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      render 'index'
+    else
+      render 'index'
+    end
   end
 
 
